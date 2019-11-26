ddc03c24145aa8bdb8233abd66a6c20094e395da
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2019-11-26 13:51:14 +0100

    Loops opdracht 1 af

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,21 @@
 //javascript code
+
+/*
+Schrijf een programma dat loopt door de getallen 1 t/m 5 met een for-loop.
+
+A)	Alle getallen toon je onder elkaar op het scherm via index.html. <br>
+B)	Alle getallen toon je met behulp van console.log() in je console
+
+
+for(statement 1; statement 2; statement 3) {
+
+}
+teller++
+teller = teller + 1;
+teller += 1;
+*/
+
+for (var teller = 1; teller <= 5; teller++) {
+    console.log(teller);
+    document.getElementById("resultaat").innerHTML += teller;
+}
