41b33ca0a881ca507be2ee47924ed697fbbad18c
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2019-12-03 11:16:00 +0100

    loops opdracht 6 af

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -2,6 +2,7 @@
 
 /*
 
+
 Maak een programma die werkt als een simpele rekenmachine: Als de variabele myOperator een * is dan moeten de varaibelen myNumber1 en myNumber2 met elkaar worden vermenigvuldigd, Als de variabele myOperator een / is moeten de varaibelen myNumber1 en myNumber2 door elkaar gedeeld worden.
 
 A)	Toon de som en de uitkomst op scherm via index.html. Dit moet er uitzien als (natuurlijk wel met je eigen nummers): <br>
