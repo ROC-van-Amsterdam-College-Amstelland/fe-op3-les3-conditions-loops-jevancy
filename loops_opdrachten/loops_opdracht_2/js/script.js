cf3b1212f53ee7fed92d084b5339a4da6f167615
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2019-12-03 22:05:13 +0100

    loops opdracht 2 af

--- loops_opdrachten/loops_opdracht_2/js/script.js
+++ loops_opdrachten/loops_opdracht_2/js/script.js
@@ -1,1 +1,21 @@
 //javascript code
+
+/*
+Schrijf een programma dat afloopt van
+20 tot 0 met behulp van een for-loop.
+
+A)	Alle getallen toon je onder elkaar op het scherm via index.html. <br>
+B)	Alle getallen toon je met behulp van console.log() in je console
+
+for(statement 1; statement 2; statement 3) {
+
+}
+teller++
+teller = teller + 1;
+teller += 1;
+*/
+
+for (var teller = 1; teller <= 20; teller++) {
+    console.log(teller);
+    document.getElementById("resultaat").innerHTML += teller;
+}
