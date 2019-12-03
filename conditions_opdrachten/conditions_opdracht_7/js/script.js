a9080cd1c2faf8bdd7b3f409163f1a9d8e06e0b5
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2019-12-03 12:54:27 +0100

    opdracht 7 loops af

--- conditions_opdrachten/conditions_opdracht_7/js/script.js
+++ conditions_opdrachten/conditions_opdracht_7/js/script.js
@@ -8,23 +8,24 @@ A)	De output toon op het scherm via index.html.<br>
 B)  Leg via comments in je javascript uit wat je doet, een comment maak je door // voor je code te zetten
 C)  ik maak een rekenmachine die kan optellen en aftrekken
 
+
 */
 
-var myOperator = "+";
+var myOperator = "-";
 var myNumber1 = prompt("geef aub een getal op?");
 var myNumber2 = prompt("geef aub een getal op?");
 var resultaat;
 
-if( myOperator == "+" ){
+if( myOperator == "-" ){
     //hier komt code
-    resultaat = Number(myNumber1) + Number(myNumber2);
+    resultaat = Number(myNumber1) - Number(myNumber2);
     document.getElementById("resultaat").innerHTML = resultaat;
 
     // console.log("De operator is " + myOperator);
 
-} else if ( myOperator == "-" ) {
+} else if ( myOperator == "*" ) {
     // console.log("De operator is -"); 
-    resultaat = myNumber1 - myNumber2;
+    resultaat = myNumber1 * myNumber2;
     document.getElementById("resultaat").innerHTML = resultaat;
 
 }
