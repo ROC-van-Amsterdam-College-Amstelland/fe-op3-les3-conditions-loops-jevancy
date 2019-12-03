dacda72bc9466274a6e73976132e723d1036fe24
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2019-12-03 11:37:25 +0100

    begin gemaakt aan opdracht 7 loops

--- conditions_opdrachten/conditions_opdracht_7/js/script.js
+++ conditions_opdrachten/conditions_opdracht_7/js/script.js
@@ -1,1 +1,30 @@
 //javascript code
+
+/*
+
+Bedenk zelf een programma waarbij je met een if, else if, else statement verschillende waardes op het scherm laat zien.
+
+A)	De output toon op het scherm via index.html.<br>
+B)  Leg via comments in je javascript uit wat je doet, een comment maak je door // voor je code te zetten
+C)  ik maak 
+
+*/
+
+var myOperator = "+";
+var myNumber1 = prompt("geef aub een getal op?");
+var myNumber2 = prompt("geef aub een getal op?");
+var resultaat;
+
+if( myOperator == "+" ){
+    //hier komt code
+    resultaat = Number(myNumber1) + Number(myNumber2);
+    document.getElementById("resultaat").innerHTML = resultaat;
+
+    // console.log("De operator is " + myOperator);
+
+} else if ( myOperator == "-" ) {
+    // console.log("De operator is -"); 
+    resultaat = myNumber1 - myNumber2;
+    document.getElementById("resultaat").innerHTML = resultaat;
+
+}
