1582d96f5a719decb973287c1afe9d92a46db121
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2019-12-05 15:33:01 +0100

    loops opdracht 10 af

--- loops_opdrachten/loops_opdracht_10/js/script.js
+++ loops_opdrachten/loops_opdracht_10/js/script.js
@@ -1,1 +1,21 @@
 //javascript code
+
+/*
+Schrijf een programma dat bij elke iteratie (ronde) een andere letter op het scherm zet. <br>
+Gebruik maximaal 5 verschillende letters. (hint: zoek op internet naar een array)
+*/
+
+var abc = ["A","B","C","D","E"];
+
+// console.log(); // A
+
+for(var teller = 0; teller < abc.length; teller++) {
+    
+    
+    console.log(abc[teller]);
+
+
+}
+
+
+
