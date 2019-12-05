e1ac2b4cc72b4b90008d9ae294980a7439276bc4
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2019-12-05 14:45:50 +0100

    loops opdracht 5 af

--- loops_opdrachten/loops_opdracht_5/js/script.js
+++ loops_opdrachten/loops_opdracht_5/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+
+/*
+Schrijf een programma dat loopt door de getallen 1 t/m 20 met een for- loop.
+
+A)	Toon alleen de <b>even</b> getallen op het scherm via index.html. <br>
+B)	Toon alleen de <b>oneven</b> getallen console.log() in je console
+
+*/
+
+for( var teller = 1 ;  teller <= 20  ; teller = teller + 1 ){
+    console.log(teller);
+    document.getElementById("resultaat").innerHTML += teller;
+}
